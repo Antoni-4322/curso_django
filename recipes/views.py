272981d67d581dd97abe3339527834d6e01d797d
Antonio Sousa <antoniosousaferreirq@gmail.com>
2024-05-23 14:25:36 -0300

    foi criado a header, separado por parciais

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Antonio Sousa'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Antonio Sousa'})
 
 
 
